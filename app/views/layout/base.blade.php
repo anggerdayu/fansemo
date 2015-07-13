b554cdb3e323e72098383e17182496c647d8fad0
Vendera Hadi <vendera@printerous.com>
2015-07-13 23:05:46 +0700

    change sementara path jquery

--- app/views/layout/base.blade.php
+++ app/views/layout/base.blade.php
@@ -153,7 +153,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="{{ asset('../bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/jquery.jscroll.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
