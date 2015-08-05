6605653a92342c6223037076bc5f16487d48f6f2
Vendera Hadi <vendera@printerous.com>
2015-08-05 23:20:22 +0700

    update post comment

--- app/views/post.blade.php
+++ app/views/post.blade.php
@@ -68,6 +68,7 @@
                 <!-- The container for the uploaded files -->
                 <div id="files" class="files"></div>
           </div>
+          @if(Auth::user())
           <form id="form-comment" action="{{url('insertcomment')}}">
           <div class="comment-type attack-bg"></div>
           <div class="comment-type assist-bg"></div>
@@ -82,6 +83,7 @@
           <br>
           <div class="pull-right"><button class="btn btn-default">Submit</button></div>
           </form>
+          @endif
           <br><br>
           <ul class="nav nav-tabs">
             <li role="presentation" class="comment-tab active"><a href="#">All</a></li>
@@ -134,9 +136,11 @@
                   @endif
                   
                   <div class="row">
+                    @if(Auth::user())
                     <div class="col-sm-12">
                       <p class="mt10"><button class="reply-comment">Reply this comment</button></p>
                     </div>
+                    @endif
                     <div class="col-sm-12 hidden">
                       <span class="btn btn-success fileinput-button">
                           <i class="glyphicon glyphicon-plus"></i>
@@ -186,67 +190,8 @@
               @endforeach
             @endif
           </div>
-<!-- <div class="row">
-                <div class="col-sm-3">
-                  <img src="{{asset('images/user.jpg')}}">
-                </div>
-                <div class="col-sm-9">
-                  <b>semar</b> &nbsp;&nbsp;<font color="#888">20 likes, 2 dislikes</font>
-                  <div class="pull-right"><button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-up"></i></button>
-              <button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-down"></i></button>&nbsp;&nbsp;<a href=""><img src="{{asset('images/attack.png')}}" width="30"></a></div>
-                  <br><br>
-                  <p>Bitch please, we won the champions league</p>
-                  <img src="{{asset('images/dummy/comment1.jpg')}}" width="400">
-                </div>
-              </div>
-              <br><br>
-              <div class="row">
-                <div class="col-sm-3">
-                  <img src="{{asset('images/user.jpg')}}">
-                </div>
-                <div class="col-sm-9">
-                  <b>tukangrusuh</b> &nbsp;&nbsp;<font color="#888">2 likes, 20 dislikes</font>
-                  <div class="pull-right"><button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-up"></i></button>
-              <button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-down"></i></button>&nbsp;&nbsp;<a href=""><img src="{{asset('images/defense.png')}}" width="30"></a></div>
-                  <br><br>
-                  <p>Congratz for your reward</p>
-                  <img src="{{asset('images/dummy/comment2.jpg')}}" width="400">
-                  commentlagi
-                  <div class="row">
-                    <br><br>
-                    <div class="col-sm-3">
-                      <img src="{{asset('images/user.jpg')}}" width="50">
-                    </div>
-                    <div class="col-sm-9">
-                      <p><b>Gerry commented :</b><br> dafuq</p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <br><br>
-                    <div class="col-sm-3">
-                      <img src="{{asset('images/user.jpg')}}" width="50">
-                    </div>
-                    <div class="col-sm-9">
-                      <p><b>Jerry commented :</b><br> hell yeah</p>
-                    </div>
-                  </div>
 
-                </div>
-              </div>
-              <br><br>
-              <div class="row">
-                <div class="col-sm-3">
-                  <img src="{{asset('images/user.jpg')}}">
-                </div>
-                <div class="col-sm-9">
-                  <b>tukangrusuh</b> &nbsp;&nbsp;<font color="#888">12 likes, 0 dislikes</font>
-                  <div class="pull-right"><button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-up"></i></button>
-              <button class="btn btn-default"><i class="glyphicon glyphicon-thumbs-down"></i></button>&nbsp;&nbsp;<a href=""><img src="{{asset('images/assist.png')}}" width="30"></a></div>
-                  <br><br>
-                  <p>Heelllppp</p>
-                  <img src="{{asset('images/dummy/comment3.jpg')}}" width="400">
-                </div>
-              </div> -->
+        <button class="btn btn-default btn-block"> Show more comments</button>
 
         
         </div>
